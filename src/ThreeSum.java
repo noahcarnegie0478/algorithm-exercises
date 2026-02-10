ab461a978eb5170347e487d7f24d74311fbc126b
noahcarnegie0478 <noah.carnegie0478@outlook.com>
2026-02-10 21:57:46 +1000

    three sum

---
+++ src/ThreeSum.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ThreeSum {
+
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> listOfLists = new HashSet<>();
+        int sizeLength = nums.length;
+        if (sizeLength < 3)
+            return new ArrayList(listOfLists);
+        int left = 0;
+        int right = 0;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < sizeLength - 2; i++) {
+
+            left = i + 1;
+            right = nums.length - 1;
+
+            while (right > left) {
+                int expectation = nums[i] + nums[left] + nums[right];
+                if (expectation == 0) {
+                    List<Integer> newArray = Arrays.asList(nums[i], nums[left], nums[right]);
+                    listOfLists.add(newArray);
+                    right--;
+                    left++;
+                } else if (expectation < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return new ArrayList(listOfLists);
+    }
+}
