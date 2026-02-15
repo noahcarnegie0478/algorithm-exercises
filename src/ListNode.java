5168356d5eaa2a448ef75008a5a607b10fc8940c
noahcarnegie0478 <noah.carnegie0478@outlook.com>
2026-02-15 23:10:15 +1000

    reverse list node

---
+++ src/ListNode.java
@@ -0,0 +1,15 @@
+
+public class ListNode {
+
+    public ListNode(Object val2) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ListNode(Object val2, ListNode head) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Object val;
+    public ListNode next;
+
+}
